55d8f787273297d8e395bfa06e9b88608338e61b
SaraAboElenien <minalona383@gmail.com>
2025-01-03 14:35:53 +0200

    Updated PostForm to use Axios instead of fetch

--- src/_root/pages/PostDetails.jsx
+++ src/_root/pages/PostDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import Loader from "@/components/shared/Loader";
+import Loader from "@/components/Shared/Loader";
 import { multiFormatDateString } from "@/lib/utils";
 import { UserContext } from "@/Context/UserContext";
 import GridPostList from "@/components/Shared/GridPostList";
