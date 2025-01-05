32c905094b9dc14790bb92d3e3bbad9c9d7b160d
SaraAboElenien <minalona383@gmail.com>
2025-01-05 13:59:59 +0200

    Refactor LeftSidebar components

--- src/_root/RootLayout.jsx
+++ src/_root/RootLayout.jsx
@@ -4,33 +4,18 @@ import Bottombar from "@/components/Shared/Bottombar";
 import LeftSidebar from "@/components/Shared/LeftSidebar";
 import React from "react";
 
-// const RootLayout = () => {
-//   return (
-//     <div className="w-full h-screen flex flex-col">
-//       <Topbar />
-
-//       <div className="flex flex-1 overflow-hidden">
-//         <LeftSidebar className="leftsidebar" />
-
-//         <section className="flex-1 overflow-y-auto custom-scrollbar">
-//           <Outlet />
-//         </section>
-//       </div>
-
-//       <Bottombar />
-//     </div>
-//   );
-// };
-
 const RootLayout = () => {
   return (
-    <div className="w-full md:flex">
+    <div className="w-full h-screen flex flex-col">
       <Topbar />
-      <LeftSidebar />
 
-      <section className="flex flex-1 h-full">
-        <Outlet />
-      </section>
+      <div className="flex flex-1 overflow-hidden">
+        <LeftSidebar className="leftsidebar" />
+
+        <section className="flex-1 overflow-y-auto custom-scrollbar">
+          <Outlet />
+        </section>
+      </div>
 
       <Bottombar />
     </div>
