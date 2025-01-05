f2a40581246e1d8aa4f52e8ee586868aafe54d51
SaraAboElenien <minalona383@gmail.com>
2025-01-05 13:54:49 +0200

    Refactor RootLayout and LeftSidebar components for improved structure and readability

--- src/_root/RootLayout.jsx
+++ src/_root/RootLayout.jsx
@@ -4,18 +4,33 @@ import Bottombar from "@/components/Shared/Bottombar";
 import LeftSidebar from "@/components/Shared/LeftSidebar";
 import React from "react";
 
+// const RootLayout = () => {
+//   return (
+//     <div className="w-full h-screen flex flex-col">
+//       <Topbar />
+
+//       <div className="flex flex-1 overflow-hidden">
+//         <LeftSidebar className="leftsidebar" />
+
+//         <section className="flex-1 overflow-y-auto custom-scrollbar">
+//           <Outlet />
+//         </section>
+//       </div>
+
+//       <Bottombar />
+//     </div>
+//   );
+// };
+
 const RootLayout = () => {
   return (
-    <div className="w-full h-screen flex flex-col">
+    <div className="w-full md:flex">
       <Topbar />
+      <LeftSidebar />
 
-      <div className="flex flex-1 overflow-hidden">
-        <LeftSidebar className="leftsidebar" />
-
-        <section className="flex-1 overflow-y-auto custom-scrollbar">
-          <Outlet />
-        </section>
-      </div>
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
 
       <Bottombar />
     </div>
